256ed8e04cc1725f0a81d4855a7b3bbf29cdd2ff
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2016-03-07 09:54:38 -0500

    setting up requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
+from os.path import join, dirname, abspath
 
-setup(name='pgsession',
-      version='0.0.dev0',
-      description='',
-      url='http://github.com/baldur/pgsession',
-      author='',
-      author_email='baldur.gudbjornsson@gmail.com',
-      license='MIT',
-      packages=['pgsession'],
-      zip_safe=False)
+
+def main():
+    reqs_file = join(dirname(abspath(__file__)), 'requirements.txt')
+    with open(reqs_file) as f:
+        requirements = [req.strip() for req in f.readlines()]
+
+    setup(name='pgsession',
+          version='0.0.dev0',
+          description='',
+          url='http://github.com/baldur/pgsession',
+          author='',
+          author_email='baldur.gudbjornsson@gmail.com',
+          license='MIT',
+          packages=['pgsession'],
+          install_requires=requirements,
+          zip_safe=False)
+
+if __name__ == '__main__':
+    main()
